db5c16a8bce7c91cd6986dde9ed9064ad84f7dba
Facundo Peovich <peovichfacundo@gmail.com>
2023-09-10 18:06:17 -0300

    version final 1

--- main.js
+++ main.js
@@ -15,24 +15,35 @@ function recuperarCarritoDeLocalStorage() {
 }
 
 // Función para manejar el clic en el botón "Añadir a Carrito"
-function addToCart(productoId) {
+async function addToCart(productoId) {
     const producto = document.getElementById(productoId);
     const nombre = producto.querySelector('h2').textContent;
     const precio = obtenerPrecioProducto(productoId);
-    const cantidad = parseInt(producto.querySelector('input').value); // Capturar la cantidad
+    const cantidad = parseInt(producto.querySelector('input').value);
 
-    // Agregar el producto al carrito
-    carrito.push({ id: productoId, nombre, precio, cantidad });
-    guardarCarritoEnLocalStorage(); // Guardar en localStorage
+    // Simulación de una solicitud al servidor para verificar la disponibilidad
+    const disponibilidad = await verificarDisponibilidadEnServidor(productoId);
 
-    // Mostrar una notificación de producto añadido al carrito
-    Swal.fire({
-        icon: 'success',
-        title: 'Producto añadido al carrito',
-        text: `${nombre} ha sido añadido al carrito.`,
-        showConfirmButton: false,
-        timer: 1500
-    });
+    if (disponibilidad) {
+        // Agregar el producto al carrito
+        carrito.push({ id: productoId, nombre, precio, cantidad });
+        guardarCarritoEnLocalStorage(); // Guardar en localStorage
+
+        Swal.fire({
+            icon: 'success',
+            title: 'Producto añadido al carrito',
+            text: `${nombre} ha sido añadido al carrito.`,
+            showConfirmButton: false,
+            timer: 1500
+        });
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: `El producto ${nombre} no está disponible en este momento.`,
+            confirmButtonText: 'Entendido'
+        });
+    }
 }
 
 // Función para obtener el precio del producto basado en su ID
@@ -43,14 +54,26 @@ function obtenerPrecioProducto(productoId) {
     return precioNumerico;
 }
 
-// Agregar eventos a los botones "Añadir a Carrito"
-const botonesAgregar = document.querySelectorAll('.bg-blue-500');
-botonesAgregar.forEach((boton) => {
-    boton.addEventListener('click', () => {
-        const productoId = boton.parentElement.id;
-        addToCart(productoId);
-    });
-});
+// Cargar los productos desde el archivo JSON
+function cargarProductosDesdeJSON() {
+    fetch('productos.json')
+        .then((response) => response.json())
+        .then((productos) => {
+            productos.forEach((producto) => {
+                // Agregar eventos a los botones "Añadir a Carrito"
+                const botonAgregar = document.getElementById(producto.id).querySelector('.bg-blue-500');
+                botonAgregar.addEventListener('click', () => {
+                    addToCart(producto.id);
+                });
+            });
+        })
+        .catch((error) => {
+            console.error('Error al cargar productos desde JSON:', error);
+        });
+}
+
+// Llama a la función para cargar los productos desde el archivo JSON
+cargarProductosDesdeJSON();
 
 // Evento para el botón "Finalizar Compra"
 const botonFinalizar = document.getElementById('finalizar');
@@ -70,7 +93,7 @@ botonFinalizar.addEventListener('click', () => {
 
         carrito.forEach((producto) => {
             const subtotal = producto.precio * producto.cantidad;
-            resumen += `${producto.nombre} - Cantidad: ${producto.cantidad} - Precio Unitario: $${producto.precio.toFixed(2)} - Subtotal: $${subtotal.toFixed(2)}\n\n`; // Agregar \n\n para separar productos
+            resumen += `${producto.nombre} - Cantidad: ${producto.cantidad} - Precio Unitario: $${producto.precio.toFixed(2)} - Subtotal: $${subtotal.toFixed(2)}\n\n`;
             total += subtotal;
         });
 
@@ -78,15 +101,15 @@ botonFinalizar.addEventListener('click', () => {
 
         Swal.fire({
             title: 'Resumen de la Compra',
-            html: `<pre>${resumen}</pre>`, // Elemento <pre> para mantener los saltos de línea
-            confirmButtonText: 'Finalizar Compra'
+            html: `<pre>${resumen}</pre>`,
+            confirmButtonText: 'Siguiente',
         }).then((result) => {
             if (result.isConfirmed) {
                 // Limpiar el carrito y restablecer los campos de cantidad
-                carrito.length = 0; // Limpiar el carrito
+                carrito.length = 0;
                 const inputsCantidad = document.querySelectorAll('input[type="number"]');
                 inputsCantidad.forEach((input) => {
-                    input.value = '1'; // Restablecer el valor predeterminado
+                    input.value = '1';
                 });
 
                 // Limpiar localStorage
@@ -96,7 +119,75 @@ botonFinalizar.addEventListener('click', () => {
     }
 });
 
+// Función simulada para verificar la disponibilidad en el servidor 
+async function verificarDisponibilidadEnServidor(productoId) {
+    return new Promise((resolve) => {
+        // Se simula una demora de 2 segundos de respuesta para con el servidor.
+        setTimeout(() => {
+            resolve(true); // Simulamos que el producto está disponible // Siempre va a dar true por el momento.
+        }, 2000);
+    });
+}
+
+// Función para mostrar el formulario de checkout
+function mostrarCheckout() {
+    const checkoutSection = document.getElementById('checkout');
+    checkoutSection.classList.remove('hidden');
+}
+
+// Función para ocultar el formulario de checkout
+function ocultarCheckout() {
+    const checkoutSection = document.getElementById('checkout');
+    checkoutSection.classList.add('hidden');
+}
+
+// Evento para el botón "Finalizar Compra"
+botonFinalizar.addEventListener('click', () => {
+    if (carrito.length === 0) {
+        // Mostrar mensaje de error si el carrito está vacío
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Tu carrito de compras está vacío.',
+            confirmButtonText: 'Entendido'
+        });
+    } else {
+        // Mostrar el formulario de checkout
+        mostrarCheckout();
+    }
+});
+
+// Evento para el envío del formulario de checkout
+const checkoutForm = document.getElementById('checkout-form');
+checkoutForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Obtener los valores del formulario
+    const nombre = document.getElementById('nombre').value;
+    const tarjeta = document.getElementById('tarjeta').value;
+
+    // Una vez que la compra se ha completado con éxito, puedes limpiar el carrito
+    carrito.length = 0;
+    const inputsCantidad = document.querySelectorAll('input[type="number"]');
+    inputsCantidad.forEach((input) => {
+        input.value = '1';
+    });
+
+    // Limpiar localStorage
+    localStorage.removeItem('carrito');
+
+    // Ocultar el formulario de checkout
+    ocultarCheckout();
+
+    // Mostrar mensaje de éxito
+    Swal.fire({
+        icon: 'success',
+        title: 'Compra Exitosa',
+        text: 'Gracias por tu compra.',
+        confirmButtonText: 'Entendido',
+    });
+});
+
 // Al cargar la página, recupera el carrito de LocalStorage
 recuperarCarritoDeLocalStorage();
-
 
